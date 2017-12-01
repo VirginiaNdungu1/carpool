40e879c6b308c41820a28ecd3b042aed83747e35
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-01 17:43:01 +0300

    Initial Project Setup

---
+++ car/car_driver/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CarDriverConfig(AppConfig):
+    name = 'car_driver'
---
+++ car/car_driver/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def drive(request):
+    return render(request, 'drive.html')
