eec73106882e8cc46486f41efe3b62006918bebf
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-04 08:21:29 +0300

    drivers app routing functionality

--- car/car_driver/urls.py
+++ car/car_driver/urls.py
@@ -4,5 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$', views.drive, name='drive'),
+    url(r'^$', views.drive, name='drive'),
+    url(r'^profiles/edit/', views.activateDriver, name='activatedriver')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
