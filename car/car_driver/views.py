f707c6e7e2d1ed897112699ec71f9b1609c68eb7
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-10 00:04:19 +0300

    update activate_driver view function

--- car/car_driver/views.py
+++ car/car_driver/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from car import settings
 from django.db import transaction
 from django.contrib import messages
+from .models import DriverProfile
 from django.contrib.auth.decorators import login_required
 from .forms import DriverForm, DriverProfileForm
 from django.contrib.auth.models import User, Group
@@ -21,9 +22,10 @@ def activateDriver(request):
         driver_profile = DriverProfileForm(
             request.POST, request.FILES, instance=request.user.profile)
         if form.is_valid() and driver_profile.is_valid():
+            form.save()
+            driver_profile.save(commit=False)
             instance.groups.add(Group.objects.get(
                 name=settings.DRIVER_GROUP_NAME))
-            form.save()
             driver_profile.save()
             messages.success(request, 'Profile successfully updated')
             return redirect(drive)
