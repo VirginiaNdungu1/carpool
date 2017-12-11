d5c3d620459b5b3c49dc358205780fedb0a109e2
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-11 09:27:34 +0300

    update travel plan function

--- car/car_driver/views.py
+++ car/car_driver/views.py
@@ -4,7 +4,7 @@ from django.db import transaction
 from django.contrib import messages
 from .models import DriverProfile
 from django.contrib.auth.decorators import login_required, user_passes_test
-from .forms import DriverForm, DriverProfileForm
+from .forms import DriverForm, DriverProfileForm, TravelPlanForm
 from django.contrib.auth.models import User, Group
 # Create your views here.
 
@@ -44,7 +44,59 @@ def activateDriver(request):
 
     return render(request, 'profiles/profile.html', {'form': form, 'driver_profile': driver_profile})
 
+#
+# def is_driver(request):
+#     user = request.user
+#     return user.groups.filter(name='drivers').exists()
 
-def is_driver(request):
+
+@login_required(login_url='/accounts/login')
+@user_passes_test(is_driver)
+def create_travelplan(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = TravelPlanForm(request.POST,)
+        if current_user.is_authenticated and form.is_valid():
+            travel_plan = form.save(commit=False)
+            travel_plan.user = current_user
+            travel_plan.save()
+            return redirect(index)
+        else:
+            form = TravelPlanForm()
+    return render(request, 'travel_plan.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login')
+@user_passes_test(is_driver)
+def travelplan_history(request, user_id):
     user = request.user
-    return user.groups.filter(name='drivers').exists()
+    travelplans = TravelPlan.objects.get(user_id=user.id)
+    return render('travel_history.html', {"travelplans": travelplans})
+
+
+@login_required(login_url='/accounts/login')
+@user_passes_test(is_driver)
+def update_travelplan(request, id):
+    create_travelplan = False
+    user = request.user
+    driver_id = user.id
+    travel_plan = TravelPlan.objects.get(id=id)
+    if request.method == 'POST':
+        form = TravelPlanForm(request.POST, obj=TravelPlan)
+        if user.is_authenticated and form.is_valid():
+            travel_plan.name = form.cleaned_data['name']
+            travel_plan.roads = form.cleaned_data['roads']
+            travel_plan.depart = form.cleaned_data['depart']
+            travel_plan.alight = form.cleaned_data['alight']
+            travel_plan.current_location = form.cleaned_data['current_location']
+            travel_plan.capacity = form.cleaned_data['capacity']
+            travel_plan.created_at = datetime.now()
+            travel_plan.user_id = driver_id
+            travel_plan.save()
+            messages.success(request, 'Profile successfully updated')
+            return redirect(travelplan_history, driver_id)
+        else:
+            messages.error(request, 'Error while updating ,,, try again')
+    else:
+        form = TravelPlanForm()
+    return render(request, 'update_travelplan.html', {"form": form})
