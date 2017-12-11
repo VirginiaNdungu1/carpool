f5c56a40524f209f79adc5f431c8c906d39b3080
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-11 12:23:00 +0300

    add urls * create_pickupoints * create_travelplan * update_travelplan * travelplan_history

--- car/car_driver/urls.py
+++ car/car_driver/urls.py
@@ -5,7 +5,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.drive, name='drive'),
-    url(r'^profiles/edit/', views.activateDriver, name='activatedriver')
+    url(r'^profiles/edit/', views.activateDriver, name='activatedriver'),
+    url(r'^pickupoint/$', views.create_pickupoints, name='pickupoint'),
+    url(r'^travelplan/$', views.create_travelplan, name='create_travelplan'),
+    url(r'^travelplan/edit/(\d+)$',
+        views.update_travelplan, name='update_travelplan'),
+    url(r'^travelplan/history/(\d+)$',
+        views.travelplan_history, name='travelplan_history'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
